7a52b6925d8860252aff24c6066447ea4c77c459
isomer04 <isomer008@gmail.com>
2023-08-03 10:53:48 -0400

    Added tailwindcss config files

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,21 @@
-import './App.css';
-import RegistrationForm from './components/RegistrationForm';
-import LoginForm from './components/LoginForm';
-import MessageForm from './components/MessageForm';
-import MessageList from './components/MessageList';
-import UserMessages from './components/UserMessages';
+import "./App.css";
+import RegistrationForm from "./components/RegistrationForm";
+import LoginForm from "./components/LoginForm";
+import MessageForm from "./components/MessageForm";
+import MessageList from "./components/MessageList";
+import UserMessages from "./components/UserMessages";
 
 function App() {
   return (
     <div className="App">
       <RegistrationForm />
       <LoginForm />
+      <hr />
       <MessageForm />
+      <hr />
       <MessageList />
+
+      <h1>User Messages</h1>
       <UserMessages />
     </div>
   );
