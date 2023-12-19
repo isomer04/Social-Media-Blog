b14046852d37c77d13586729555c9c9ff5217b93
isomer04 <43922158+isomer04@users.noreply.github.com>
2023-12-18 23:01:35 -0500

    Added conditional rendering for showing posted by and human-readable time

--- client/src/components/Message.jsx
+++ client/src/components/Message.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import axios from 'axios';
+import { useParams } from 'react-router-dom';
+
 
 const Message = ({ message, currentUserId, onUpdate, onDelete }) => {
+  const { username } = useParams();
+
   const handleUpdate = () => {
-    // Add your logic for handling the update here
     console.log('Update message:', message);
     onUpdate(message);
   };
@@ -22,8 +25,17 @@ const Message = ({ message, currentUserId, onUpdate, onDelete }) => {
   return (
     <div className="bg-white p-4 border border-gray-300 rounded shadow mb-4">
       <p className="text-lg mb-1">{message.message_text}</p>
-      <p className="text-gray-600 text-sm mb-1">Posted by: {message.posted_by}</p>
-      <p className="text-gray-600 text-sm">Posted at: {message.time_posted_epoch}</p>
+      
+      {/* Conditionally render posted_by based on the pathname */}
+      {username && (
+        <p className="text-gray-600 text-sm mb-1">
+          Posted by: {username || message.posted_by}
+        </p>
+      )}
+      
+      <p className="text-gray-600 text-sm">
+        Posted at: {new Date(message.time_posted_epoch * 1000).toLocaleString()}
+      </p>
 
       {canEditAndDelete && (
         <div className="flex mt-2">
